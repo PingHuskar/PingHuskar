b5c835358540d97d81ec0fdeccb5359ab639dcaf
PingHuskar <ping47024@icloud.com>
2022-06-16 16:35:41 +0700

    webProject to script

---
+++ webProject.js
@@ -0,0 +1,16 @@
+const webList = [
+'https://fantastic-biscochitos-6543f1.netlify.app/?phone=0857758787',
+'https://pinghuskar.github.io/Kipchumba',
+'https://pinghuskar.github.io/easy-search-app/',
+'https://pinghuskar.github.io/Covid-Kalaland/',
+'https://pinghuskar.github.io/health-record-app/',
+'https://pinghuskar.github.io/html-form-template/',
+'',
+'',
+]
+
+for (var i=0;i<webList.length;i++) {
+    if (webList[i] !== "") {
+    document.getElementById('webProject').innerHTML += `<section data-background-iframe="${webList[i]}" data-background-interactive></section>`
+    }
+}
